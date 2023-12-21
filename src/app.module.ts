d9ac9d9bcdbe63c28ed6a6d33f79d08977f648e2
cmvn-quangtran <duyquang.tdq7@gmail.com>
2023-12-21 08:34:57 +0700

    crud users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'nestjs.mysql.database.azure.com',
+      port: 3306,
+      username: 'admin123',
+      password: 'duyquang@2301',
+      database: 'nestjs-app',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
